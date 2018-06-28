8748469e3d0b23c4bdc76c5445783823f4ff4dc5
Urielink <urielink81@gmail.com>
2018-06-28 15:40:36 -0500

    Listo, menu simplificado

--- inc/themeNavbars.php
+++ inc/themeNavbars.php
@@ -38,94 +38,171 @@ function logoTheme() {
 
 function ekilineNavbar($navPosition){
 
-	$navTop = get_theme_mod('ekiline_topmenuSettings');
-    $navMain = get_theme_mod('ekiline_primarymenuSettings');
-    $navMod = get_theme_mod('ekiline_modalNavSettings');
-	
-	if ($navTop || $navMain || $navMod == '0') {
-	    $navAction = ' static-top';
-    } else if ($navTop || $navMain || $navMod == '1') {
-        $navAction = ' fixed-top'; 
-    } else if ($navTop || $navMain || $navModp == '2') {
-        $navAction = ' fixed-bottom'; 
-    } else if ($navTop || $navMain || $navMod == '3') {
-        $navAction = ' navbar-sticky'; 
-    }	
-    
-	if( true === get_theme_mod('ekiline_inversemenu') ){
-	    $inverseMenu = 'navbar-dark bg-dark'; 
-    } else {
-        $inverseMenu = 'navbar-light bg-light';
-    }
-	
-	$navTopStyle = get_theme_mod('ekiline_topmenuStyles');
-	$navMainStyle = get_theme_mod('ekiline_primarymenuStyles');
-	$navModStyle = get_theme_mod('ekiline_modalNavStyles');
-	
-	if ($navTopStyle || $navMainStyle || $navModStyle == '0') {
-	    $navAlign = ' mr-auto';
-		$navHelper = '';
-	    $showModal = '';
-    } else if ($navTopStyle || $navMainStyle || $navModStyle == '1') {
-        $navAlign = ''; 
-		$navHelper = ' justify-content-md-center';
-        $showModal = 'move-from-bottom'; 
-    } else if ($navTopStyle || $navMainStyle || $navModStyle == '2') {
-        $navAlign = ' ml-auto'; 
-		$navHelper = '';
-        $showModal = 'left-aside'; 
-    } else if ($navTopStyle || $navMainStyle || $navModStyle == '3') {
-        $showModal = 'right-aside'; 
-    } 		
-	
-	// if ( has_nav_menu( $navPosition ) || has_nav_menu( $navPosition ) || has_nav_menu( $navPosition ) ){
-// 
-		// if ($navPosition == 'top') : echo 'menu superior'; endif;
-		// if ($navPosition == 'primary') : echo 'menu normal'; endif;
-		// if ($navPosition == 'modal') : echo 'menu modal'; endif;
-// 		
-	// }
-
-	if ( has_nav_menu( $navPosition ) || has_nav_menu( $navPosition ) || has_nav_menu( $navPosition ) ){
-	
-		if ( has_nav_menu( 'top' ) ) : ?>
+	if ( has_nav_menu( $navPosition ) ){
 		
-		<nav id="site-navigation-top"  class="navbar <?php echo $inverseMenu;?> navbar-expand-md top-navbar<?php echo $navAction;?>">
-		    <div class="container">
+		// invertir color (class css)
+        $inverseMenu = 'navbar-light bg-light ';		
+		if( true === get_theme_mod('ekiline_inversemenu') ) : $inverseMenu = 'navbar-dark bg-dark ';  endif;
+				
+		// Variables por cada tipo de menu: configurcion y distribucion de menu	    						
+		if ($navPosition == 'top'){
+			$actions = get_theme_mod('ekiline_topmenuSettings'); 
+			$styles = get_theme_mod('ekiline_topmenuStyles'); 
+		}
+		if ($navPosition == 'primary'){
+			$actions = get_theme_mod('ekiline_primarymenuSettings');
+			$styles = get_theme_mod('ekiline_primarymenuStyles'); 
+		}
+		if ($navPosition == 'modal'){
+			$actions = get_theme_mod('ekiline_modalNavSettings');
+			$styles = ''; // estos estilos se invocan en el fragmento de modal al final
+		}				
+		
+		//Clases css por configuración de menu
+		if ($actions == '0') {
+		    $navAction = 'static-top';
+	    } elseif ($actions == '1') {
+	        $navAction = 'fixed-top'; 
+	    } elseif ($actions == '2') {
+	        $navAction = 'fixed-bottom'; 
+	    } elseif ($actions == '3') {
+	        $navAction = 'navbar-sticky'; 
+	    }	
+				
+		//Clases css por estilo de menu
+		/*tipos de animacion: .zoom, .newspaper, .move-horizontal, .move-from-bottom, .unfold-3d, .zoom-out, .left-aside, .right-aside */
+		if ($styles == '0') {
+		    $navAlign = 'mr-auto ';
+			$navHelper = '';
+	    } else if ($styles == '1') {
+	        $navAlign = ''; 
+			$navHelper = ' justify-content-md-center ';
+	    } else if ($styles == '2') {
+	        $navAlign = 'ml-auto '; 
+			$navHelper = '';
+	    }
+		
+		// Clases css para mostrar el boton del modal
+		if ($navPosition != 'modal'){ $expand = 'navbar-expand-md '; } else { $expand = ' '; }
+		
+		// Clases reunidas para <nav>
+		$navClassCss = 'navbar '. $inverseMenu . $expand . $navPosition . '-navbar ' . $navAction;
+		
+		// variables para boton modal
+		$dataToggle = 'collapse';
+		$dataTarget = '.navbar-collapse.'.$navPosition;				
+		if ($navPosition == 'modal'): $dataToggle = 'modal'; $dataTarget = '#navModal'; endif; 
+			
 	
-	            <h2><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php logoTheme(); ?></a></h2>
-	                        
-	            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse.top">
-	      			<!--span class="navbar-toggler-icon"></span--><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-	            </button>
+		 ?>
+
+			<nav id="site-navigation-<?php echo $navPosition; ?>"  class="<?php echo $navClassCss;?>">
+			    <div class="container">
+		
+		            <h2><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php logoTheme(); ?></a></h2>
 	
-		        
-		        <div id="navbar-collapse-out" class="collapse navbar-collapse top<?php echo $navHelper;?>">
-	
-				<span class="navbar-text d-none d-sm-block"><?php echo get_bloginfo( 'description' ); ?></span>        	
-		        
-	    	        <?php wp_nav_menu( 
-	                        array(
-	        	                'menu'              => 'top',
-	        	                'theme_location'    => 'top',
-	        	                'depth'             => 2,
-	        	                'container'         => '',
-	        	                'menu_class'        => 'navbar-nav'.$navAlign,
-	        	                'menu_id'           => 'top-menu',
+					<?php if ( $navPosition == 'primary' || $navPosition == 'modal' ) { ?> 
+					<span class="navbar-text d-none d-sm-block"><?php echo get_bloginfo( 'description' ); ?></span> 
+					<?php } ?>
+										
+		            <button class="navbar-toggler collapsed" type="button" data-toggle="<?php echo $dataToggle; ?>" data-target="<?php echo $dataTarget; ?>">
+		      			<!--span class="navbar-toggler-icon"></span--><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+		            </button>
+
+			        <?php if ($navPosition == 'top') { ?>
+			        	
+				        <div id="navbar-collapse-out" class="collapse navbar-collapse <?php echo $navPosition; ?><?php echo $navHelper;?>">
+			
+						<span class="navbar-text d-none d-sm-block"><?php echo get_bloginfo( 'description' ); ?></span>        	
+								        
+			    	        <?php wp_nav_menu( array(
+			        	                'menu'              => 'top',
+			        	                'theme_location'    => 'top',
+			        	                'depth'             => 2,
+			        	                'container'         => '',
+		                                'container_class'   => '',
+		                                'container_id'      => '',
+			        	                'menu_class'        => 'navbar-nav '.$navAlign,
+			        	                'menu_id'           => 'top-menu',
+			                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+			        	                'walker'            => new WP_Bootstrap_Navwalker()
+			    	                ) ); ?>
+			    	
+			    			<?php dynamic_sidebar( 'navwidget-nw1' ); ?>     
+				        
+				        </div>
+				        
+		        	<?php } elseif ($navPosition == 'primary') { ?>	
+		        		
+	                    <?php wp_nav_menu( array(
+	                            'menu'              => 'primary',
+	                            'theme_location'    => 'primary',
+	                            'depth'             => 2,
+	                            'container'         => 'div',
+                                'container_class'   => 'collapse navbar-collapse primary'.$navHelper,
+                                'container_id'      => 'navbar-collapse-in',
+	                            'menu_class'        => 'navbar-nav '.$navAlign,
+	                            'menu_id'           => 'main-menu',
 	                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-	        	                'walker'            => new WP_Bootstrap_Navwalker()
-	    	                )
-	    	              ); ?>
-	    	
-	    			<?php dynamic_sidebar( 'navwidget-nw1' ); ?>     
-		        
-		        </div>
-		    </div><!-- .container -->         
-		</nav><!-- .site-navigation -->         
-		<?php endif; 
-	
-	}	
-	
-	
-	
-} 
+	                            'walker'            => new WP_Bootstrap_Navwalker()
+	                            ) ); ?>                           
+	                    
+	            		<?php dynamic_sidebar( 'navwidget-nw2' ); ?>     
+		        			        
+		        	<?php } ?>		        
+			    </div><!-- .container -->         
+			</nav><!-- .site-navigation -->        
+
+	<?php } //endif;
+
+}
+
+/*
+ * Fragmento para crear un menu con madal
+ */
+
+function ekiline_modalMenuBottom(){
+	if ( !has_nav_menu( 'modal' ) )	return;	
+	//tipos de animacion: .zoom, .newspaper, .move-horizontal, .move-from-bottom, .unfold-3d, .zoom-out, .left-aside, .right-aside		
+	switch ( get_theme_mod('ekiline_modalNavStyles') ) {
+	    case 0 : $showModal = ''; break;
+	    case 1 : $showModal = 'move-from-bottom '; break;
+	    case 2 : $showModal = 'left-aside '; break;
+	    case 3 : $showModal = 'right-aside '; break;
+	}
+
+	{ //template ?> 
+<div id="navModal" class="modal fade <?php echo $showModal;?>" tabindex="-1" role="dialog" aria-labelledby="navModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="navModalLabel"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+    <?php wp_nav_menu( array(
+                'menu'              => 'modal',
+                'theme_location'    => 'modal',
+                'depth'             => 2,
+                'container'         => 'nav',
+                'container_class'   => 'modal-body',
+                'container_id'      => '',
+                'menu_class'        => 'navbar-nav',
+                'menu_id'           => 'modal-menu',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker()
+            ) ); ?>
+    			
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php } // fin template
+
+}
+add_action( 'wp_footer', 'ekiline_modalMenuBottom', 0 );
+
